ffbc05beb1e01fc97bfcd847ba7eb84f5fb49d33
porrametict <60021414@up.ac.th>
2020-07-26 19:51:22 +0700

    update serializer

--- course/serializers.py
+++ course/serializers.py
@@ -1,14 +1,38 @@
 from course import models
 from rest_framework import serializers
 
-class  TeachingPeriodSerializer(serializers.ModelSerializer):
-    class Meta :
+
+class TeachingPeriodSerializer(serializers.ModelSerializer):
+    class Meta:
         fields = '__all__'
         model = models.TeachingPeriod
-    
+
+
 class CourseSerializer(serializers.ModelSerializer):
-    teaching_period_data = TeachingPeriodSerializer(read_only=True,source='teaching_period')
-    class Meta :
+    teaching_period = TeachingPeriodSerializer()
+
+    class Meta:
         fields = '__all__'
         model = models.Course
-    
+
+    def create(self, validated_data):
+        teaching_period = validated_data.pop('teaching_period')
+        teaching_period = models.TeachingPeriod.objects.create(**teaching_period)
+        course = models.Course.objects.create(teaching_period=teaching_period, **validated_data)
+        return course
+
+    def update(self, instance, validated_data):
+        teaching_period_data = validated_data.pop('teaching_period')
+        teaching_period = instance.teaching_period
+
+        instance.name = validated_data.get('name', instance.name)
+        instance.status = validated_data.get('teacher_name', instance.teacher_name)
+        instance.status = validated_data.get('status', instance.status)
+        instance.save()
+
+        teaching_period.start_time = teaching_period_data.get('start_time', teaching_period.start_time)
+        teaching_period.end_time = teaching_period_data.get('end_time', teaching_period.end_time)
+        teaching_period.day = teaching_period_data.get('day', teaching_period.day)
+        teaching_period.save()
+
+        return  instance
--- course/views.py
+++ course/views.py
@@ -1,31 +1,8 @@
-from django.shortcuts import render
 from course import models, serializers
-from rest_framework import viewsets, status
-from rest_framework.response import Response
+from rest_framework import viewsets
 
 
 # Create your views here.
 class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.CourseSerializer
     queryset = models.Course.objects.all()
-
-    def create(self, request, *args, **kwargs):
-        teaching_period_serializer = serializers.TeachingPeriodSerializer(data=request.data)
-        if teaching_period_serializer.is_valid():
-            teaching_period_serializer.save()
-            new_data = request.data
-            # new_data = new_data.dict()   # fix => AttributeError: 'dict' object has no attribute 'dict'
-            new_data['teaching_period'] = teaching_period_serializer.data['id']
-            course_data = self.save_corse(new_data)
-            print(course_data)
-            return Response(course_data, status=status.HTTP_200_OK)
-        else:
-            return Response(teaching_period_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def save_corse(self, data):
-        course_serializer = serializers.CourseSerializer(data=data)
-        if course_serializer.is_valid():
-            course_serializer.save()
-            return course_serializer.data
-        else:
-            return course_serializer.errors
