3632dc385c69d1ddbb259d2cb1bb8f4251009e80
DESKTOP-CFQ1PK9\Anony <ford19982541@gmail.com>
2020-08-11 12:22:29 +0700

    Dec.ford : Add filter in student.

--- student/views.py
+++ student/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+
+from django_filters.rest_framework import DjangoFilterBackend
 from student import serializers, models
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 
 
 # Create your views here.
 class StudentViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.StudentSerializers
     queryset = models.Student.objects.all().order_by('-id')
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['course_id']
